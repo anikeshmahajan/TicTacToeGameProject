976fcaea67e9d3e9990c899c4f02a0a708d32a33
Anikesh <anikesh@0725@gmail.com>
2021-04-25 14:49:29 +0530

    Addition : Uc4 Selection of Index

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,18 +1,20 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-  
+
 	public static void main(String[] args) {
 		
-		
-			TicTacToeGame game = new TicTacToeGame();
-			Scanner scanner = new Scanner(System.in);
-			board();
+		    Scanner scanner = new Scanner(System.in);
+			
+			 char[] board=board();
 			char userLetter=chooseLetter(scanner);
 			char compLetter=(userLetter =='x')?'o' :'x';
-			char[] c = board();
-			showBoard(c);
 			
+			showBoard(board);
+		    int userMove=	indexChooser(board,scanner);
+		    board[userMove]=userLetter;
+		    showBoard(board);
 			
 		}	
 		
@@ -30,6 +32,7 @@ public class TicTacToeGame {
 		}
 	static char chooseLetter(Scanner scanner)
 	{
+	
 		System.out.println("Choose the Letter o or x");
 		char choosedLetter =scanner.next().toLowerCase().charAt(0);
 		
@@ -46,16 +49,35 @@ public class TicTacToeGame {
 			
 		
 	}
-	 static void showBoard( char gameBoard[])
+	 static void showBoard( char board[])
 	{
 		    System.out.println(" ");
-	        System.out.println("\n "+ gameBoard[1] + " | "+ gameBoard[2] + " | " +gameBoard[3]);
+	        System.out.println("\n "+ board[1] + " | "+ board[2] + " | " +board[3]);
 	        System.out.println("---------");
-	        System.out.println(" "+ gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6]);
+	        System.out.println(" "+ board[4] + " | " + board[5] + " | " + board[6]);
 	        System.out.println("---------");
-	        System.out.println(" "+ gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9]);
+	        System.out.println(" "+ board[7] + " | " + board[8] + " | " + board[9]);
 	        System.out.println(" ");
 	}
 	 
-	 
+	 static int indexChooser(char board[],Scanner scanner)
+	   {
+	      
+	        Integer[] validCells = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+
+	        while (true)
+	        {
+			System.out.println("What is your next move ? (1-9");
+			int index = scanner.nextInt();
+	            	if (Arrays.asList(validCells).contains(index) && isSpaceFree(board, index))
+				
+	            		return index;
+		}
+
+	    }
+	    private static boolean isSpaceFree(char[] board, int index)
+	    {
+	        return board[index] == '\0';
+	    }
 }
